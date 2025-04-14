4f8c5f26c89b3baa079f8b383f35a5a26121eaac
EyalMuttath <168302861+EyalMuttath@users.noreply.github.com>
2025-04-14 19:09:29 +0300

    new hello

--- Hello.java
+++ Hello.java
@@ -3,7 +3,7 @@ public class Hello{
         //System.out.println("new change");
 
         for(int i=1;i<=10;i++){
-            System.out.println("*not* Hello World..."+i);
+            System.out.println(" *not* Hello World..."+i);
         }
     }
 }
